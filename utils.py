fix a bug in rotation.

utils.py
@@ -26,13 +26,14 @@ class UpperAndLowerCenterCrop(object):
         half_width = center_r * 1.05  # height = width
         img = img.crop((int(center_x - half_width), int(center_y - half_width),
                         int(center_x + half_width), int(center_y + half_width)))
+        # 生成mask覆盖非检测区域
         size = img.size
         circle = np.zeros(size, dtype='uint8')  # 黑色背景
         cv2.circle(circle, (size[0] // 2, size[1] // 2), size[0] // 2, 1, -1)  # 中心圆内不改变
         circle = np.stack((circle,) * 3, -1)  # 扩展维度 gray to rgb
-        img_np = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-        img_np = img_np * circle
-        img = Image.fromarray(np.array(cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)))
+        img_np = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)  # 黑白照片其实不需要RGB2BGR
+        img_np = img_np * circle  # 0去除1留存
+        img = Image.fromarray(np.array(cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)))  # 黑白照片其实不需要RGB2BGR
 
         return img
 
@@ -53,6 +54,15 @@ class SideCenterCrop(object):
         half_width = center_r * 1.05  # height = width
         img = img.crop((int(center_x - half_width), int(center_y - half_width),
                         int(center_x + half_width), int(center_y + half_width)))
+        # 生成mask覆盖非检测区域
+        size = img.size
+        circle = np.zeros(size, dtype='uint8')  # 黑色背景
+        cv2.circle(circle, (size[0] // 2, size[1] // 2), size[0] // 2, 1, -1)  # 中心圆内不改变
+        circle = np.stack((circle,) * 3, -1)  # 扩展维度 gray to rgb
+        img_np = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)  # 黑白照片其实不需要RGB2BGR
+        img_np = img_np * circle  # 0去除1留存
+        img = Image.fromarray(np.array(cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)))  # 黑白照片其实不需要RGB2BGR
+
         return img
 
 
@@ -72,6 +82,15 @@ class ChamferCenterCrop(object):
         half_width = center_r * 1.05  # height = width
         img = img.crop((int(center_x - half_width), int(center_y - half_width),
                         int(center_x + half_width), int(center_y + half_width)))
+        # 生成mask覆盖非检测区域
+        size = img.size
+        circle = np.zeros(size, dtype='uint8')  # 黑色背景
+        cv2.circle(circle, (size[0] // 2, size[1] // 2), size[0] // 2, 1, -1)  # 中心圆内不改变
+        circle = np.stack((circle,) * 3, -1)  # 扩展维度 gray to rgb
+        img_np = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)  # 黑白照片其实不需要RGB2BGR
+        img_np = img_np * circle  # 0去除1留存
+        img = Image.fromarray(np.array(cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)))  # 黑白照片其实不需要RGB2BGR
+
         return img
 
 