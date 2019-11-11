44864ec0495c1b81aca170b86ebf3bc1cab70845
caltong <caltong1027@gmail.com>
2019-11-11 22:31:07 +0800

    add weights to entropy loss, may change entropy loss to focal loss later.

--- side_model.py
+++ side_model.py
@@ -117,7 +117,7 @@ model_ft.fc = torch.nn.Linear(num_ftrs, 2)
 
 model_ft = model_ft.to(device)
 
-criterion = torch.nn.CrossEntropyLoss()
+criterion = torch.nn.CrossEntropyLoss(weight=torch.tensor([0.9, 0.1]).to(device))
 
 # Observe that all parameters are being optimized
 optimizer_ft = torch.optim.SGD(model_ft.parameters(), lr=lr, momentum=0.9)
