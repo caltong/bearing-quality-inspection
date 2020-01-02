10972fb52190e324ea8dcd059a340728f5a069ce
caltong <caltong1027@gmail.com>
2020-01-02 20:47:33 +0800

    add background generate method

---
+++ inpaint.py
@@ -0,0 +1,22 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2
+import json
+
+img_id = '12060_053'
+img = cv2.imread(img_id + '.bmp')
+with open(img_id + '.json') as f:
+    json_labels = json.load(f)
+
+labels = []
+for i in json_labels['shapes']:
+    labels.append(i['points'])
+
+# 创建mask
+mask = np.zeros((img.shape[0], img.shape[1]), np.uint8)
+for i in labels:
+    cv2.fillPoly(mask, [np.array(i, np.int32)], (255, 0, 0))
+
+result = cv2.inpaint(img, mask, 3, cv2.INPAINT_NS)
+plt.imshow(result)
+plt.show()
