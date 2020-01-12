c822f558ed25a7c50b4ecf56ab627b88ac0fec8f
caltong <caltong1027@gmail.com>
2020-01-12 18:36:34 +0800

    adjust labels by center and radius of target circle

--- generate.py
+++ generate.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 from PIL import Image
 from utils import get_radius_center
+from time import time
 
 
 def generate_new_data(image_path, labels_path):
@@ -24,14 +25,33 @@ def generate_new_data(image_path, labels_path):
 
     # 获取工件中心坐标和半径
     center, radius = get_radius_center(Image.fromarray(background))
+    left_up_corner = np.array(center) - radius
     # 获取损伤目标小图
+    new_labels = []
     for i in labels:
-        np_label = np.array(i, np.int32)
-        np_label
-    new_image = Image.fromarray(background)
+        a_np_label = np.array(i, np.int32)
+        plus_len = a_np_label.shape[0]
+        plus = np.repeat([left_up_corner], plus_len, axis=0)
+        a_np_label = a_np_label - plus
+        new_labels.append(a_np_label)
+
+    # 获取原图裁切目标图 带有损伤信息
+    origin_crop_image = image_cv2[center[1] - radius:center[1] + radius,
+                        center[0] - radius:center[0] + radius]
+
+
+
+    # crop (left, top, right, bottom)
+    new_image = Image.fromarray(background).crop((center[0] - radius,
+                                                  center[1] - radius,
+                                                  center[0] + radius,
+                                                  center[1] + radius))
     return new_image
 
 
 if __name__ == "__main__":
+    tic = time()
     new_image = generate_new_data("inpaint-sample/12060_942.bmp", "inpaint-sample/12060_942.json")
+    toc = time()
+    print(toc - tic)
     new_image.show()
