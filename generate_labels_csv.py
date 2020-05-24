6f7d0ead3c36fa4875b88b04462e267edb098100
caltong <caltong1027@gmail.com>
2020-05-24 14:27:02 +0800

    generate_labels_csv

--- generate_labels_csv.py
+++ generate_labels_csv.py
@@ -1,5 +1,26 @@
 import pandas as pd
 import os
 
+# 获取所有文件名
 files = os.listdir("test")
-print(files)
+# 创建list
+json_list = []
+image_list = []
+for file in files:
+    if file.endswith(".json"):
+        json_list.append(file)
+    else:
+        image_list.append(file)
+
+# 写入csv的数据
+# [文件名, 正负样本, json名]
+data = []
+for image in image_list:
+    one_data = [image, 0]
+    json = image.split(".")[0] + ".json"
+    if json in json_list:
+        one_data.append(json)
+    else:
+        one_data.append("None")
+    data.append(one_data)
+print(data)
